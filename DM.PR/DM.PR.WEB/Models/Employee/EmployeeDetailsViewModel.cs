485efb0c692190e5b0fc375e409931b30b270321
MoskalevDenchik <moskalev.denchik@mail.ru>
2018-06-17 22:35:01 +0300

    HF-4.6.5: change EmployeesDetails viewModel

--- DM.PR/DM.PR.WEB/Models/Employee/EmployeeDetailsViewModel.cs
+++ DM.PR/DM.PR.WEB/Models/Employee/EmployeeDetailsViewModel.cs
@@ -9,14 +9,16 @@ namespace DM.PR.WEB.Models
     {
         public int Id { get; set; }
 
-        public int? DepartmentId { get; set; }
-
+        [Display(Name = "Отдел")]
         public string DepartmentName { get; set; }
 
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Отчество")]
         public string MiddleName { get; set; }
 
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
 
         [Display(Name = "Домашний телефон")]
@@ -28,19 +30,25 @@ namespace DM.PR.WEB.Models
         [Display(Name = "Мобильный телефон")]
         public string MobilePhone { get; set; }
 
+        [Display(Name = "E-mails")]
         public List<Email> Emails { get; set; }
 
+        [Display(Name = "Домашний адресс")]
         public string Address { get; set; }
 
+        [Display(Name = "Семейное положение")]
         public string MaritalStatus { get; set; }
 
+        [Display(Name = "Статус")]
         public string WorkStatus { get; set; }
-
+                                                      
         public string ImagePath { get; set; }
 
+        [Display(Name = "Дата приема на работу")]
         [DataType(DataType.Date)]
         public DateTime? BeginningOfWork { get; set; }
 
+        [Display(Name = "Дата увольнения")]
         [DataType(DataType.Date)]
         public DateTime? EndOfWork { get; set; }
     }
