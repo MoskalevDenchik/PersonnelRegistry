eb33cc0da000a8bbbf8bfec668022798dabde8c5
MoskalevDenchik <moskalev.denchik@mail.ru>
2018-06-22 20:17:57 +0300

    HF-4.8.6: add phones script

--- DM.PR/DM.PR.WEB/Scripts/Department/Phones.js
+++ DM.PR/DM.PR.WEB/Scripts/Department/Phones.js
@@ -1,4 +1,4 @@
-﻿phoneNumber = 0;
+﻿phoneNumber = 1;
 $("#AddPhone").on('click', function ()
 {
     AddNewPhone();
@@ -6,16 +6,16 @@ $("#AddPhone").on('click', function ()
 
 function AddNewPhone()
 {
-    $.ajax({
-        url: "/Phone/AddPhone",
-        type: "GET",
-        data: {
-            number: phoneNumber
-        },
-        success: function (respons)
-        {
-            $("#Phones").append(respons);
-            phoneNumber++;
-        }
-    })
+    var copy = $("#Phones div:first").clone();
+
+    copy.find("span:first").attr("data-valmsg-for", "Phones[" + phoneNumber + "].Number");
+
+    copy.find("input[type='hidden']").attr("name", "Phones[" + phoneNumber + "].Id");
+    copy.find("input[type='hidden']").attr("id", "Phones" + phoneNumber + "__Id");
+
+    copy.find("input[type='text']").attr("name", "Phones[" + phoneNumber + "].Number");
+    copy.find("input[type='text']").attr("id", "Phones" + phoneNumber + "__Number");
+
+    copy.appendTo("#Phones");
+    phoneNumber++;
 }
