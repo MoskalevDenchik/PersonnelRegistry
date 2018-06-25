6f8e71b75a582bb51da7a76043f6a78f236e6518
MoskalevDenchik <moskalev.denchik@mail.ru>
2018-06-25 10:36:38 +0300

    HF-4.9.7: add authentication with ajax

--- DM.PR/DM.PR.WEB/Controllers/AccountController.cs
+++ DM.PR/DM.PR.WEB/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
-﻿using DM.PR.Common.Entities.Account;
-using DM.PR.WEB.Models.Account;
+﻿using DM.PR.WEB.Infrastructure.Attributes;
 using DM.PR.Business.Services;
 using System.Web.Mvc;
 
@@ -7,60 +6,36 @@ namespace DM.PR.WEB.Controllers
 {
     public class AccountController : Controller
     {
-        ILoginServices _loginServices;
+        #region Private
 
+        private readonly ILoginServices _loginServices;
+
+        #endregion
+
+        #region Ctors
         public AccountController(ILoginServices loginServices)
         {
             _loginServices = loginServices;
         }
-        public ActionResult Login(string returnUrl)
+
+        #endregion
+
+        public ActionResult Login()
         {
-            ViewBag.ReturnUrl = returnUrl;
             return View();
         }
 
+        [AjaxOnly]
         [HttpPost]
-        public ActionResult Login(LoginViewModel model, string returnUrl)
+        public JsonResult Login(string Login, string Password)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            SignInStatus result = _loginServices.SignIn(model.Login, model.Password);
-
-            switch (result)
-            {
-                case SignInStatus.Success:
-                    return RedirectToAction("Index", "Manage");
-                case SignInStatus.InvalidPssword:
-                    ModelState.AddModelError("", "Вы ввели неверный пароль");
-                    return View(model);
-                case SignInStatus.Failure:
-                default:
-                    ModelState.AddModelError("", "Пользователя с таким именем не существует");
-                    return View(model);
-            }
+            return Json(_loginServices.SignIn(Login, Password));
         }
 
-        public ActionResult LoginOut(string returnUrl)
+        public ActionResult Logout()
         {
             _loginServices.SingOut();
             return RedirectToAction("Index", "Home");
         }
-
-
-        #region Helpers
-
-        private ActionResult RedirectToLocal(string returnUrl)
-        {
-            if (Url.IsLocalUrl(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            return RedirectToAction("Index", "Home");
-        }
-
-        #endregion
     }
 }
